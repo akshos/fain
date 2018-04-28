ad50606a9a2309cf3b3fa90a286876a34f65cc24
Akshay Venugopal <kannanakshayvenugopal@gmail.com>
2018-04-28 11:33:07 +0530

    Table create

---
+++ Fain/src/database/Tables.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+/**
+ *
+ * @author akshos
+ */
+public final class Tables {
+    public static final String masterTable = "create table master("
+            + "accountNo    varchar(20) primary key,"
+            + "";
+}
