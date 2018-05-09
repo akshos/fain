7816c23244049fb4110871cfbd064ac314adda4c
lenovo <lenovo@Another>
2018-05-09 12:35:25 +0530

    db select one done except purchase latex and sales

--- Fain/src/database/StockDB.java
+++ Fain/src/database/StockDB.java
@@ -78,18 +78,17 @@ public final class StockDB {
         return rs;
     }
     
-    public static ResultSet selectOneId(Statement stmt, String id){
+    public static String[] selectOneId(Statement stmt, String id){
         String sql="select * from stock where itemCode="+id+";";
         ResultSet rs=null;
-        ResultSet rs1=null;
         try{
             rs=stmt.executeQuery(sql);
-            rs1=rs;
+            return ResultSetToStringArray.getRowAsStringArray(rs);
         }
         catch(SQLException se){
             se.printStackTrace();
         }
-        return rs1;
+        return null;
     }
     
     public static String[][] getItems(Statement stmt){
